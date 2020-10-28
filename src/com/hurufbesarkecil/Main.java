5986288941269fc95de0ab8bfba8c3e7672ba5e1
Ivan Faathirza <49663880+ivan0811@users.noreply.github.com>
2020-10-28 12:08:47 +0700

    initial commit

--- src/com/hurufbesarkecil/Main.java
+++ src/com/hurufbesarkecil/Main.java
@@ -27,5 +27,6 @@ public class Main {
 
     public static void main(String[] args) {
         tampilKalimat(masukkanKalimat());
+        System.out.println("Developed by : Ivan Faathirza");
     }
 }
